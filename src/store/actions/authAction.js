c930618b96c5f8c473411efbd61735a39ef9ebd6
QuangSon1901 <quangsonbmt760@gmail.com>
2023-02-02 11:20:36 +0700

    ADF

--- src/store/actions/authAction.js
+++ src/store/actions/authAction.js
@@ -34,7 +34,7 @@ export const userLogin = (data) => {
     return async (dispatch) => {
         const config = {
             headers: {},
-            withCredentials: true,
+            credentials: 'include',
         };
         try {
             const res = await axios.post(process.env.REACT_APP_URL_API + '/messenger/user-login', data, config);
